c418332d86db2b4754abf4eacb5574aacdcd8d76
Alejandro Matias <alejandro.matias@hotmail.com>
2024-02-01 14:55:07 -0600

    Se migro los valores de una clase a otra

--- src/Components/Formulario/Index.js
+++ src/Components/Formulario/Index.js
@@ -1,1 +1,97 @@
 export {default} from './Formulario';
+
+export const options = [
+
+    { id: 'AGS', value: "AGS", label: "Aguascalientes" },
+    { id: 'BC', value: "BC", label: "Baja California" },
+    { id: 'BCS', value: "BCS", label: "Baja California Sur" },
+    { id: 'CAM', value: "CAM", label: "Campeche" },
+    { id: 'CHI', value: "CHI", label: "Chihuahua" },
+    { id: 'CHS', value: "CHS", label: "Chiapas" },
+    { id: 'COA', value: "COA", label: "Coahuila" },
+    { id: 'COL', value: "COL", label: "Colima" },
+    { id: 'DF', value: "DF", label: "Distrito Federal" },
+    { id: 'DUR', value: "DUR", label: "Durango" },
+    { id: 'GRO', value: "GRO", label: "Guerrero" },
+    { id: 'GTO', value: "GTO", label: "Guanajuato" },
+    { id: 'HID', value: "HID", label: "Hidalgo" },
+    { id: 'JAL', value: "JAL", label: "Jalisco" },
+    { id: 'MCH', value: "MCH", label: "Michoacan" },
+    { id: 'MEX', value: "MEX", label: "Estado de México" },
+    { id: 'MOR', value: "MOR", label: "Morelos" },
+    { id: 'NAY', value: "NAY", label: "Nayarit" },
+    { id: 'NL', value: "NL", label: "Nuevo León" },
+    { id: 'OAX', value: "OAX", label: "Oaxaca" },
+    { id: 'PUE', value: "PUE", label: "Puebla" },
+    { id: 'QR', value: "QR", label: "Quintana Roo" },
+    { id: 'QUE', value: "QUE", label: "Queretaro" },
+    { id: 'SIN', value: "SIN", label: "Sinaloa" },
+    { id: 'SLP', value: "SLP", label: "San Luis Potosi" },
+    { id: 'SON', value: "SON", label: "Sonora" },
+    { id: 'TAB', value: "TAB", label: "Tabasco" },
+    { id: 'TAM', value: "TAM", label: "Tamaulipas" },
+    { id: 'TLA', value: "TLA", label: "Tlaxcala" },
+    { id: 'VER', value: "VER", label: "Veracruz" },
+    { id: 'YUC', value: "YUC", label: "Yucatan" },
+    { id: 'ZAC', value: "ZAC", label: "Zacatecas" },
+
+];
+
+export const opcionRegimeen=[
+    {id:'601',value:"601",label:"General de Ley Personas Morales"},
+    {id:'603',value:"603",label:"Personas Morales con Fines no Lucrativos"},
+    {id:'605',value:"605",label:"Sueldos y Salarios e Ingresos Asimilados a Salarios"},
+    {id:'606',value:"606",label:"Arrendamiento"},
+    {id:'607',value:"607",label:"Régimen de Enajenación o Adquisición de Bienes"},
+    {id:'608',value:"608",label:"Demás ingresos"},
+    {id:'610',value:"610",label:"Residentes en el Extranjero sin Establecimiento Permanente en México"},
+    {id:'611',value:"611",label:"Ingresos por Dividendos (socios y accionistas)"},
+    {id:'612',value:"612",label:"Personas Físicas con Actividades Empresariales y Profesionales"},
+    {id:'614',value:"614",label:"Ingresos por intereses"},
+    {id:'615',value:"615",label:"Régimen de los ingresos por obtención de premios"},
+    {id:'616',value:"616",label:"Sin obligaciones fiscales"},
+    {id:'620',value:"620",label:"Sociedades Cooperativas de Producción que optan por diferir sus ingresos"},
+    {id:'621',value:"621",label:"Incorporación Fiscal"},
+    {id:'622',value:"622",label:"Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras"},
+    {id:'623',value:"623",label:"Opcional para Grupos de Sociedades"},
+    {id:'624',value:"624",label:"Coordinados"},
+    {id:'625',value:"625",label:"Régimen de las Actividades Empresariales con ingresos a través de Plataformas Tecnológicas"},
+    {id:'626',value:"626",label:"Régimen Simplificado de Confianza"}
+
+];
+
+export const RazonSocial =[
+    {id:'D',value:"D",label:"DermaExpress"},
+    {id:'C',value:"C",label:"Call Center"},
+    {id:'A',value:"A",label:"Alicia"}
+
+];
+
+export const UsoPrincipal=[
+    {id:'G01',value:"Adquisición de mercancías",label:"Adquisición de mercancías"},
+    {id:'G02',value:"Devoluciones, descuentos o bonificaciones",label:"Devoluciones, descuentos o bonificaciones"},
+    {id:'G03',value:"Gastos en general ",label:"Gastos en general "},
+    {id:'H01',value:"Construcciones ",label:"Construcciones "},
+    {id:'I02',value:"Mobiliario y equipo de oficina por inversiones ",label:"Mobiliario y equipo de oficina por inversiones "},
+    {id:'I03',value:"Equipo de transporte ",label:"Equipo de transporte "},
+    {id:'I04',value:"Equipo de cómputo y accesorios ",label:"Equipo de cómputo y accesorios"},
+    {id:'I05',value:"Dados, troqueles, moldes, matrices y herramental ",label:"Dados, troqueles, moldes, matrices y herramental "},
+    {id:'I06',value:"Comunicaciones telefónicas ",label:"Comunicaciones telefónicas "},
+    {id:'I07',value:"Comunicaciones satelitales ",label:"Comunicaciones satelitales "},
+    {id:'I08',value:"Otra maquinaria y equipo ",label:"Otra maquinaria y equipo "},
+    {id:'D01',value:"Honorarios médicos, dentales y gastos hospitalarios. ",label:"Honorarios médicos, dentales y gastos hospitalarios. "},
+    {id:'D02',value:"Gastos médicos por incapacidad o discapacidad ",label:"Gastos médicos por incapacidad o discapacidad "},
+    {id:'D03',value:"Gastos funerales. ",label:"Gastos funerales. "},
+    {id:'D04',value:"Donativos",label:"Donativos"},
+    {id:'D05',value:"Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación). ",label:"Intereses reales efectivamente pagados por créditos hipotecarios (casa habitación). "},
+    {id:'D06',value:"Aportaciones voluntarias al SAR. ",label:"Aportaciones voluntarias al SAR. "},
+    {id:'D07',value:"Primas por seguros de gastos médicos. ",label:"Primas por seguros de gastos médicos. "},
+    {id:'D08',value:"Gastos de transportación escolar obligatoria. ",label:"Gastos de transportación escolar obligatoria. "},
+    {id:'D09',value:"Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones. ",label:"Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones. "},
+    {id:'D10',value:"Pagos por servicios educativos (colegiaturas) ",label:"Pagos por servicios educativos (colegiaturas) "},
+    {id:'CP01',value:"Pagos",label:"Pagos"},
+    {id:'CN01',value:"Nómina",label:"Nómina"},
+    {id:'S01',value:"Sin Efectos Fiscales",label:"Sin Efectos Fiscales"},
+
+];
+
